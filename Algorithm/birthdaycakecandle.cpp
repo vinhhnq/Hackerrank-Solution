545f64bc7e15356213463facd2856341a8879b6c
juniokun <vinhng74@gmail.com>
2022-03-09 12:07:01 +0700

    candle

---
+++ Algorithm/birthdaycakecandle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }
+  int biggest = 0;
+  for(int i=0;i<n;i++){
+    biggest = max(biggest, a[i]);
+  }
+  int count =0;
+  for(int i =0; i<n;i++){
+    if(biggest == a[i]){
+      count++;
+    }
+  }
+  cout<<count;
+
+  return 0;
+
+}
